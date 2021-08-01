a7a7d874ca62c78fcbd21f7c445008ed7a286195
unknown <hassan.abdallah.haa@gmail.com>
2021-08-01 20:18:42 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import React from "react";
 import UnSplash from "./api/UnSplash";
 import SearchBar from "./components/SearchBar";
 import ImageList from "./components/ImageList";
-
+import ButtonIcon from "./DarkMode/DarkMode/ButtonIcon";
 class App extends React.Component {
   state = { images: [] };
 
+  componentDidMount() {
+    this.onSearchSubmit(" lamborghini");
+  }
   onSearchSubmit = async (term) => {
     const response = await UnSplash.get("/search/photos", {
       params: { query: term },
@@ -17,6 +20,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App ui container" style={{ marginTop: "10px" }}>
+        <ButtonIcon></ButtonIcon>
         <SearchBar onSubmit={this.onSearchSubmit} />
         <ImageList images={this.state.images} />
       </div>
